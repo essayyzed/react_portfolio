afaf1e58f91729751451f3da37f889c70ec35725
essayyzed <p180034@nu.edu.pk>
2021-06-18 15:38:49 +0500

    Add css and routing to Home Page

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import Image from '/home/essayyzed/Documents/react_portfolio/src/monstera.jpg'
+
+export default function Home() {
+    return (
+        <main>
+            <img
+                src={Image}
+                alt="Monstera Leaves"
+                className="absolute object-cover w-full h-full "
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-sung home-name">An Aspiring Geek.</h1>
+            </section>
+        </main>
+    )
+}
---
+++ src/components/Post.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react"
+import {Link} from "react-router-dom"
+import { SanityClient } from "../client"
+
+export default function Post() {
+     
+        const [postData, setPost] = useState(null);
+
+        useEffect(() => {
+            SanityClient.fetch(`*[_type == "post"]{
+                title, 
+                slug, 
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    },
+                    alt
+                }
+            }`)
+            .then((data) => setPost(data))
+            .catch(console.error)
+        }, [])
+
+        return (
+            <main className="bg-green-100 min-h-screen p-12">
+                <section className="container mx-auto">
+                    <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
+                    <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to Blogs Posts</h2>
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        <article>
+                            <Link to={"/post/"+post.slug.current} key={post.slug.current}>
+                                <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" key={index}>
+                                    <img src={post.mainImage.asset.url} alt={post.mainImage.alt} 
+                                    className="w-full h-full"
+                                    />
+                                    <span>
+                                        <h3></h3>
+                                    </span>
+                                </span>
+                            </Link>
+                        </article>
+                    </div>
+                </section>
+            </main>
+        )
+}
